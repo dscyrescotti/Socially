f53582da9d8e506d62f3e0cc200cace123774bdd
Phoe Lapyae <phoelapyayt7@gmail.com>
2020-06-16 13:39:46 +0630

    Initial commit

---
+++ Example/Socially/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Socially
+//
+//  Created by phoelapyae69 on 06/16/2020.
+//  Copyright (c) 2020 phoelapyae69. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
